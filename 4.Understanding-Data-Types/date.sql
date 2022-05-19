0a02cd62f1cb24ae64e38c302163f165a135d7ac
Devere Weaver <weaverdevere@gmail.com>
2022-05-18 19:41:19 -0500

    completed data types chapter

--- 4.Understanding-Data-Types/date.sql
+++ 4.Understanding-Data-Types/date.sql
@@ -16,3 +16,10 @@ VALUES ('2022-12-31 01:00 EST', '2 days'),
 	   (now(), '1 week');
 	   
 SELECT * FROM date_time_types;
+
+
+/* Use the table to experiment with interval data types in calculations */
+SELECT timestamp_column, 
+	   interval_column,
+       timestamp_column - interval_column AS new_date    -- interval calculation 
+FROM date_time_types;
