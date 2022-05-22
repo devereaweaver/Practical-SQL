13dc3f9725aa74d8e70fb256e5c27f603d22f0fb
Devere Weaver <weaverdevere@gmail.com>
2022-05-22 12:00:26 -0500

    still working on exercies

--- 5.Importing-and-Exporting-Data/ch5-exercises.sql
+++ 5.Importing-and-Exporting-Data/ch5-exercises.sql
@@ -16,3 +16,32 @@ FROM '/Users/devere/Data-Engineering/PracticalSQL/5.Importing-and-Exporting-Data
 WITH (FORMAT CSV, HEADER, DELIMITER ':', QUOTE '#');
 
 TABLE movies;
+
+
+/* Export a to a CSV the 20 counties in the US that had the 
+ * most births. Export only county name, state, # births.
+ */
+SELECT county_name, state_name, births_2019
+FROM us_counties_pop_est_2019
+ORDER BY births_2019 DESC
+LIMIT 20;
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
